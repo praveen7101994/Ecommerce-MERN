19b867b18526abedfd4daf68c514bc99392f445d
Praveen Patel <praveenkumarpatel@convertcart.com>
2024-01-29 22:52:21 +0530

    Created product, user and order models...

---
+++ backend/config/models/productModel.js
@@ -0,0 +1,70 @@
+import mongoose from "mongoose";
+
+const reviewSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "user",
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+  },
+  comment: {
+    type: String,
+    required: true,
+  },
+});
+
+const productSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "user",
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    reviews: [reviewSchema],
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,11 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import { products } from "./data/products.js";
+import connectDB from "./config/connectDB.js";
 const port = process.env.PORT || 5000;
 const app = express();
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("API is running...");
