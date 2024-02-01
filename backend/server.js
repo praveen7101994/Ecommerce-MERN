acf70732f291d2a5e0f8afa5a922f1258847117c
Praveen Patel <praveenkumarpatel@convertcart.com>
2024-02-01 22:57:50 +0530

    added products controllers and async middleware and error middleware....

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
-import { products } from "./data/products.js";
+import productRoutes from "./routes/productRoutes.js";
 import connectDB from "./config/connectDB.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 const port = process.env.PORT || 5000;
 const app = express();
 
 connectDB();
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
   res.send("API is running...");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+app.use("/api/products", productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`server running on port ${port}`));
