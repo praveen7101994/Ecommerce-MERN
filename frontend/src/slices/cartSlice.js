593dbeec54249fd0685a764a18fff04ec999eac6
Praveen Patel <praveenpatelonline@gmail.com>
2024-06-08 23:19:00 +0530

    cart slice

---
+++ frontend/src/slices/cartSlice.js
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = localStorage.getItem("cart")
+  ? JSON.parse(localStorage.getItem("cart"))
+  : { cartItems: [] };
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      const item = action.payload;
+
+      const existItem = state.cartItems.find((x) => x._id === item._id);
+
+      if (existItem) {
+        state.cartItems = state.cartItems.map((x) =>
+          x._id === existItem._id ? item : x
+        );
+      } else {
+        state.cartItems = [...state.cartItems, item];
+      }
+
+      // calculate items price
+
+      // calculate shipping price
+      // calculate tax price
+      // calculate total price
+    },
+  },
+});
+
+export default cartSlice.reducer;
