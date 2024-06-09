96b57f5f4d2d32b7a64380566226508b74d01120
Praveen Patel <praveenpatelonline@gmail.com>
2024-06-09 17:36:41 +0530

    completing product screen with add to cart feature...

--- frontend/src/pages/ProductScreen/ProductScreen.jsx
+++ frontend/src/pages/ProductScreen/ProductScreen.jsx
@@ -1,23 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "./product-screen.css";
 import LaptopImg from "./../../images/dell5530.jpg";
 import Button from "./../../components/common/Button/Button";
 import Row from "../../components/common/Row/Row";
 import Rating from "../../components/rating/Rating";
 import Separator from "../../components/common/Separator/Separator";
-import {
-  useGetProductDetailsQuery,
-  useGetProductsQuery,
-} from "../../slices/productApiSlice";
-import { useParams } from "react-router-dom";
+import { useGetProductDetailsQuery } from "../../slices/productApiSlice";
+import { useNavigate, useParams } from "react-router-dom";
+import { addToCart } from "../../slices/cartSlice";
+import { useDispatch } from "react-redux";
 
 const ProductScreen = () => {
-  const { id: productId } = useParams;
+  const { id: productId } = useParams();
   const {
     data: product,
     isLoading,
     error,
   } = useGetProductDetailsQuery(productId);
+
+  const countInStock = product?.countInStock;
+
+  const [qty, setQty] = useState(1);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleAddToCart = () => {
+    dispatch(addToCart({ ...product, qty }));
+    navigate("/cart");
+  };
+
+  const handleQty = (e) => {
+    const value = e.target.value;
+    if (value > countInStock) {
+      alert(`Only ${countInStock} items the stock available`);
+      return;
+    }
+    setQty(e.target.value);
+  };
 
   return (
     <>
@@ -35,35 +54,48 @@ const ProductScreen = () => {
             <div className="product-detail">
               <div className="product-title">{product.name}</div>
               <Separator />
-              <Rating value={4} text={"8 reviews"} />
+              <Rating value={4} text={`${product.numReviews} reviews`} />
               <Separator />
-              <div className="product-price">Price: $1212</div>
+              <div className="product-price">Price: {`$${product.price}`}</div>
               <Separator />
-              <div className="product-desc">
-                this is the product description
-              </div>
+              <div className="product-desc">{`${product.description}`}</div>
             </div>
             <div className="cart-action">
               <div className="space-between">
-                Price:<span>$20</span>
+                Price:<span>{`$${product.price}`}</span>
               </div>
               <Separator />
               <div className="space-between">
-                Status: <span>In Stock</span>
+                Status:{" "}
+                <span>
+                  {countInStock > 0
+                    ? `In Stock (${countInStock})`
+                    : "Out of stock"}
+                </span>
               </div>
               <Separator />
               <div className="space-between">
                 Quantity:{" "}
                 <span>
-                  <input type="number" style={{ width: "40px" }} />
+                  <input
+                    type="number"
+                    onChange={handleQty}
+                    style={{ width: "40px" }}
+                    value={qty}
+                    // max={countInStock}
+                  />
                 </span>
               </div>
               <Separator />
               <div className="space-between">
-                Total Price: <span>$20</span>
+                Total Price: <span>${qty * product.price}</span>
               </div>
               <Separator />
-              <Button title={"Add To Cart"} />
+              <Button
+                onClick={handleAddToCart}
+                title={"Add To Cart"}
+                disabled={countInStock === 0 ? true : false}
+              />
             </div>
           </div>
         </Row>
