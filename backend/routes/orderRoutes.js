df0abf07946ce17a420b6551f6a6b89794176b93
Praveen Patel <praveenpatelonline@gmail.com>
2024-07-01 20:42:28 +0530

    making order route not admin protected

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -15,7 +15,7 @@ import { protect, admin } from "../middleware/authMiddleware.js";
 router.route("/").post(protect, addOrderItems).get(protect, admin, getOrders);
 router.route("/mine").get(protect, getMyOrders);
 router.route("/:id").get(protect, getOrderById);
-router.route("/:id/pay").put(protect, admin, updateOrderToPaid);
+router.route("/:id/pay").put(protect, updateOrderToPaid);
 router.route("/:id/delivered").put(protect, admin, updateOrderToDelivered);
 
 export default router;
