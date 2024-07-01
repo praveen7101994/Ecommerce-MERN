855a0f0b23905e877ced413ccb37408cffb671e7
Praveen Patel <praveenpatelonline@gmail.com>
2024-07-02 01:51:00 +0530

    Added createdAt to the order model

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -56,6 +56,11 @@ const orderSchema = mongoose.Schema(
       required: true,
       default: 0.0,
     },
+    createdAt: {
+      type: Date,
+      required: true,
+      default: Date.now,
+    },
     isPaid: {
       type: Boolean,
       required: true,
